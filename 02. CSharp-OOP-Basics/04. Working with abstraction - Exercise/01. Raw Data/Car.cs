56e90a35a02ecaecb033369e3cf4a7749d6031ae
atanaska <atanaska.kiricheva@gmail.com>
2018-10-24 18:13:20 +0300

    fix

---
+++ 02. CSharp-OOP-Basics/04. Working with abstraction - Exercise/01. Raw Data/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_RawData
+{
+    public class Car
+    {
+        private string model;
+        private int engineSpeed;
+        private int enginePower;
+        private int cargoWeight;
+        private string cargoType;
+        private KeyValuePair<double, int>[] tires;
+
+
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double tire1Pressure, int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure, int tire3age, double tire4Pressure, int tire4age)
+        {
+            this.Model = model;
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+            this.Tires = new KeyValuePair<double, int>[] { KeyValuePair.Create(tire1Pressure, tire1Age), KeyValuePair.Create(tire2Pressure, tire2Age), KeyValuePair.Create(tire3Pressure, tire3age), KeyValuePair.Create(tire4Pressure, tire4age) };
+        }
+
+        public string Model { get => model; set => model = value; }
+        public int EngineSpeed { get => engineSpeed; set => engineSpeed = value; }
+        public int EnginePower { get => enginePower; set => enginePower = value; }
+        public int CargoWeight { get => cargoWeight; set => cargoWeight = value; }
+        public string CargoType { get => cargoType; set => cargoType = value; }
+        public KeyValuePair<double, int>[] Tires { get => tires; set => tires = value; }
+    }
+}
---
+++ 02. CSharp-OOP-Basics/04. Working with abstraction - Exercise/05. Greedy Times/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+
+    public class Potato
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            string[] quantities = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+            long gold = 0;
+            long gems = 0;
+            long money = 0;
+
+            for (int i = 0; i < quantities.Length; i += 2)
+            {
+                string name = quantities[i];
+                long quantity = long.Parse(quantities[i + 1]);
+
+                string kind = string.Empty;
+
+                if (name.Length == 3)
+                {
+                    kind = "Cash";
+                }
+                else if (name.ToLower().EndsWith("gem"))
+                {
+                    kind = "Gem";
+                }
+                else if (name.ToLower() == "gold")
+                {
+                    kind = "Gold";
+                }
+
+                if (kind == "")
+                {
+                    continue;
+                }
+                else if (input < bag.Values.Select(x => x.Values.Sum()).Sum() + quantity)
+                {
+                    continue;
+                }
+
+                switch (kind)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(kind))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (quantity > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[kind].Values.Sum() + quantity > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Cash":
+                        if (!bag.ContainsKey(kind))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (quantity > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[kind].Values.Sum() + quantity > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(kind))
+                {
+                    bag[kind] = new Dictionary<string, long>();
+                }
+
+                if (!bag[kind].ContainsKey(name))
+                {
+                    bag[kind][name] = 0;
+                }
+
+                bag[kind][name] += quantity;
+                if (kind == "Gold")
+                {
+                    gold += quantity;
+                }
+                else if (kind == "Gem")
+                {
+                    gems += quantity;
+                }
+                else if (kind == "Cash")
+                {
+                    money += quantity;
+                }
+            }
+
+            foreach (var x in bag)
+            {
+                Console.WriteLine($"<{x.Key}> ${x.Value.Values.Sum()}");
+                foreach (var item2 in x.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{item2.Key} - {item2.Value}");
+                }
+            }
+        }
+    }
+}
