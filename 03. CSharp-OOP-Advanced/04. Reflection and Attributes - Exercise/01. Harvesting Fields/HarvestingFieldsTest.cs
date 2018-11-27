8018e9cb2b46bd9ad8e65be39cac7fc555da99b8
atanaska <atanaska.kiricheva@gmail.com>
2018-11-27 14:28:37 +0200

    ex

---
+++ 03. CSharp-OOP-Advanced/04. Reflection and Attributes - Exercise/01. Harvesting Fields/HarvestingFieldsTest.cs
@@ -0,0 +1,55 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            string command = Console.ReadLine();
+
+            var currentAssembly = typeof(HarvestingFields);
+
+            FieldInfo[] fields ;
+
+            while (command != "HARVEST")
+            {
+                switch (command)
+                {
+                    case "private":
+                        fields = currentAssembly.GetFields(
+                            BindingFlags.Static | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.NonPublic)
+                            .Where(x=>x.IsPrivate).ToArray();
+                        break;
+                    case "protected":
+                        fields = currentAssembly.GetFields(
+                            BindingFlags.Static | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.NonPublic)
+                            .Where(x => x.IsFamily).ToArray(); 
+                            break;
+                    case "public":
+                        fields = currentAssembly.GetFields(
+                            BindingFlags.Static | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.NonPublic)
+                            .Where(x=>x.IsPublic).ToArray();
+                        break;
+                    case "all":
+                        fields = currentAssembly.GetFields(
+                            BindingFlags.Static | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.NonPublic);
+                            break;
+                    default: throw new ArgumentException();
+                }
+                foreach (var field in fields)
+                {
+                    Console.WriteLine($"{field.Attributes.ToString().ToLower().Replace("family","protected")} {field.FieldType.Name} {field.Name}");
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 03. CSharp-OOP-Advanced/04. Reflection and Attributes - Exercise/02. Black Box Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,58 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type blackBoxType = typeof(BlackBoxInteger);
+
+            ConstructorInfo blackBoxConstructor = blackBoxType
+                .GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[0], null);
+
+            Object blackBoxInstance = blackBoxConstructor.Invoke(new object[] { });
+
+            string[] command = Console.ReadLine().Split("_");
+
+            while (command[0] != "END")
+            {
+                switch (command[0])
+                {
+                    case "Add":
+                        Operation("Add", command[1]);
+                        break;
+                    case "Subtract":
+                        Operation("Subtract", command[1]);
+                        break;
+                    case "Divide":
+                        Operation("Divide", command[1]);
+                        break;
+                    case "Multiply":
+                        Operation("Multiply", command[1]);
+                        break;
+                    case "RightShift":
+                        Operation("RightShift", command[1]);
+                        break;
+                    case "LeftShift":
+                        Operation("LeftShift", command[1]);
+                        break;
+                }
+
+                command = Console.ReadLine().Split("_");
+            }
+
+            void Operation(string methodName, string param)
+            {
+                var operation = blackBoxType
+                    .GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);//flags for private
+                operation.Invoke(blackBoxInstance, new object[] { int.Parse(command[1]) }); //obj=>method params
+
+                var field = blackBoxType.GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Console.WriteLine(field.GetValue(blackBoxInstance));
+            }
+        }
+    }
+}
